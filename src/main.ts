745c34234ae6cc0db80351d52544cf970bf92e3f
dungtd3 <dungtd3@vng.com.vn>
2022-05-22 16:23:42 +0700

    Enable prisma logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { PrismaService } from './services/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/services/prisma/prisma.service.ts
+++ src/services/prisma/prisma.service.ts
@@ -3,6 +3,24 @@ import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error']
+    })
+
+    this.$use(async (params, next) => {
+      const before = Date.now()
+
+      const result = await next(params)
+
+      const after = Date.now()
+
+      console.log(`Query ${params.model}.${params.action} took ${after - before}ms`)
+
+      return result
+    })
+  }
+
   async onModuleInit() {
     await this.$connect()
   }
